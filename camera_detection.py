d5c95c274f6ca6961a9c9ad60b77b3e07d7ffdae
Behrad Ravanbod <99behi@gmail.com>
2023-01-27 19:40:11 +0330

    Add camera feature

---
+++ camera_detection.py
@@ -0,0 +1,35 @@
+import cv2
+import imutils
+from influxdb_client import InfluxDBClient
+from influxdb_client.client.write_api import ASYNCHRONOUS
+
+
+class CameraDetector:
+    def __init__(self, detector, influxdb_url, influxdb_token, influxdb_org, influxdb_bucket_name):
+        self.detector = detector
+        if influxdb_url is not None:
+            self.influxdb_url = influxdb_url
+            self.influxdb_token = influxdb_token
+            self.influxdb_org = influxdb_org
+            self.influxdb_bucket_name = influxdb_bucket_name
+            self.influx_client = InfluxDBClient(
+                url=influxdb_url, token=influxdb_token, org=influxdb_org)
+            self.influx_write_api = self.influx_client.write_api(
+                write_options=ASYNCHRONOUS)
+
+    def process(self):
+        video = cv2.VideoCapture(0)
+        while True:
+            check, frame = video.read()
+            if check:
+                frame = imutils.resize(frame, width=min(800, frame.shape[1]))
+                frame, total_persons = self.detector.detect(frame)
+                cv2.imshow('output', frame)
+                if self.influx_client is not None:
+                    self.influx_write_api.write(self.influxdb_bucket_name, self.influxdb_org, [
+                                                "persons,location=location persons={}".format(total_persons).encode()])
+
+                if cv2.waitKey(1) == ord('q'):
+                    break
+
+        video.release()
